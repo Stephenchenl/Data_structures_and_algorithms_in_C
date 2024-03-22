335dae7530768be8918c4ee658258ca2b28ce161
chenl <3247996679@qq.com>
2024-03-22 18:20:45 +0800

    second commit 2024_3_22

--- inc/my_library.h
+++ inc/my_library.h
@@ -1,7 +1,16 @@
 #ifndef MY_LIBRARY_H
 #define MY_LIBRARY_H
 
-int* random(int a[],int length,int MaxNumber); //生成[0,MaxNumber-1]范围的随机数组
+int* random(int a[],int length); //生成[0,length+1]范围的随机数组
+
 void swap_int(int *a,int *b);    //交换两个整型变量的值
+
+void bubble_sort(int arry[],int length);   //冒泡排序
+
+/*
+快速排序
+*/
+void quicksort(int arr[], int low, int high);
+int partition(int arr[], int low, int high);
 
 #endif  //MY_LIBRARY_H
--- src/random.c
+++ src/random.c
@@ -5,12 +5,12 @@
 /*
 以时间序列为准生成随机数
 */
-int* random(int a[],int length,int MaxNumber){
+int* random(int a[],int length){
     //使用当前时间生成一个随机数种子
     srand(time(NULL));
-    //生成随机数组，模运算代表生成随机数的范围为[0,MaxNumber-1]；
+    //生成随机数组，模运算代表生成随机数的范围为[0,length+1]；
     for(int i = 0;i < length; i++){
-        a[i] = rand() % MaxNumber;
+        a[i] = rand() % (length+1);
     }
 
     return a;
